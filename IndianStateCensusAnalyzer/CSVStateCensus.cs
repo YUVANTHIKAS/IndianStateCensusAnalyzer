4a0729ccb8afe06bccc7dda474ba293843629c01
Yuvanthika Sarathy <yuvanthika.s@tvsnext.com>
2023-08-20 20:45:51 +0530

    [yuvanthika]Add:Created

---
+++ IndianStateCensusAnalyzer/CSVStateCensus.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyzer
+{
+    public class CSVStateCensus
+    {
+        public static int ReadStateCensusData(string filepath)
+        {
+            using (var reader = new StreamReader(filepath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusData>().ToList();
+                    return records.Count() - 1;
+                }
+            }
+        }
+    }
+}
---
+++ IndianStateCensusAnalyzer/StateCensusAnalyzer.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace IndianStateCensusAnalyzer
+{
+    public class StateCensusAnalyzer
+    {
+        private static TextReader reader;
+
+        public static int ReadStateCensusData(string filePath)
+        {
+           if (!Path.GetExtension(filePath).Equals(".csv"))
+            {
+                throw new CensusAnalyzerException(CensusAnalyzerException.ExceptionType.FILE_INCORRECT, "file extension incorrect");
+            }
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusData>().ToList();
+                    Console.WriteLine("Read Data from CSV");
+                    foreach (var data in records)
+                    {
+                        Console.WriteLine(data.State + "---" + data.Population + "---" +
+                            data.AreaInSqKm + "---" + data.DensityPerSqKm);
+                    }
+                    return records.Count() - 1;
+                }
+            }
+        }
+    }
+}
